e5397b7697d3fb7c8e578744a04c8b0ce1b4769d
Xin-Heng <xinheng1993@gmail.com>
2018-11-19 16:43:46 -0800

    add token

--- server.js
+++ server.js
@@ -2,16 +2,31 @@ const port = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
 const api_router = express.Router();
+let bodyParser = require('body-parser');
+const config = require('./config');
 const app_name = "<NAME>";
 
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 // API routing
-api_router.get('/generatelink', (req, res) => {
-    let roomid = Math.random()
+api_router.post('/generatelink', (req, res) => {
+    if(req.headers['token'] === config.video_token){
+        let roomid = Math.random()
         .toString(36)
         .slice(2) + Date.now();
     res.json({
         url: 'https://comp4711-video-chat.herokuapp.com/chatroom?roomid=' + roomid
-    });
+    });       
+    }else{
+        res.json(
+            {
+                error: 'Access Denied, No Token Found'
+            });
+    }
 });
 api_router.get('/testing1', (req, res) => {
     res.json({
