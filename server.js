ed6d2eb5b70b6b69356934d930c9bfb73759c7f9
shawnzheng99 <czheng24@my.bcit.ca>
2018-11-21 01:48:17 -0800

    update cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ let username = 'mo ren';
 app.all('*', function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "token");
     next();
 });
 
