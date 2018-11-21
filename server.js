5cdd5efbf7b315b669f50a4adaa652f74974b5db
shawnzheng99 <czheng24@my.bcit.ca>
2018-11-21 01:32:56 -0800

    added allow methods

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ let username = 'mo ren';
 
 app.all('*', function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
